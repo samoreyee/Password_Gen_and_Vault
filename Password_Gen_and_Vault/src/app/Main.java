72d70f73942799f85281a36dbf9fad1970c00342
leonardogalvez <leonardogalvez@localhost>
2026-09-04 17:13:30 -0400

    created a new invalid if user doesn't input need to fix the while and for

--- Password_Gen_and_Vault/src/app/Main.java
+++ Password_Gen_and_Vault/src/app/Main.java
@@ -7,25 +7,48 @@ public class Main {
 	public static void main(String[] args) {
  
 		Scanner user = new Scanner(System.in); 
-
 		
+		User newUser = null;
+		boolean running1 = true;
+	while(running1) {
 		System.out.println("Input User Name: ");
 		
 		String userName = user.nextLine();
+		
+		if (userName.isEmpty()) {
+	        System.out.println("Username cannot be empty.");
+	        
+;
+	    }
 		
 		System.out.println("Input Email: ");
 		
 		String userEmail = user.nextLine();
 		
+		if (userEmail.isEmpty()) {
+	        System.out.println("Email cannot be empty.");
+	  
+	    }
+		
 		System.out.println("Input Password: ");
 		
 		String password = user.nextLine();
 		
-		User newUser = new User(userName, userEmail, password);
+		if (password.isEmpty()) {
+	        System.out.println("Username cannot be empty.");
+	       
+	    }
+		
+		newUser = new User(userName, userEmail, password);
 
 		System.out.println("Profile Created!");
 		
 		System.out.println("********************  Welcome to LockedIn  ********************");
+		
+		running1 = false;
+
+	}
+	
 	
 		boolean running = true;
 		
@@ -44,14 +67,34 @@ public class Main {
 				System.out.println("New Credential Name: ");
 				String credentialName = user.nextLine();
 				
+				if (credentialName.isEmpty()) {
+			        System.out.println("Name cannot be empty.");
+			        continue;
+			    }
+				
 				System.out.println("New Credential User Name: ");
 				String credentialUserName = user.nextLine();
+				
+				if (credentialUserName.isEmpty()) {
+			        System.out.println("Username cannot be empty.");
+			        continue;
+			    }
 				
 				System.out.println("New Credential Email: ");
 				String credentialEmail = user.nextLine();
 				
+				if (credentialEmail.isEmpty()) {
+			        System.out.println("Email cannot be empty.");
+			        continue;
+			    }
+				
 				System.out.println("New Credential Password: "); 
 				String credentialPassword= user.nextLine();
+				
+				if (credentialPassword.isEmpty()) {
+			        System.out.println("Username cannot be empty.");
+			        continue;
+			    }
 								
 				Credential newCredential = new Credential(credentialName, credentialUserName, credentialEmail, credentialPassword);
 
@@ -66,6 +109,10 @@ public class Main {
 					newUser.displayCredentials();
 					System.out.println("Which Credential would you like to select? ");
 					String targetName = user.nextLine(); 
+					if (targetName.isEmpty()) {
+				        System.out.println("cannot be empty.");
+				        continue;
+				    }
 					
 					newUser.selectCredentials(targetName);
 					
@@ -82,7 +129,6 @@ public class Main {
 			}
 			
 		}
-		
 		
 		user.close(); 
 	}
